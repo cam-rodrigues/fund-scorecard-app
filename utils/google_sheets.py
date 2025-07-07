b0c2f699a944d4f0aa183add9027534363571323
cam-rodrigues <crods611@gmail.com>
2025-07-07 15:04:54 -0400

    Update google_sheets.py

--- utils/google_sheets.py
+++ utils/google_sheets.py
@@ -1,18 +1,20 @@
-import streamlit as st
 import gspread
-from google.oauth2.service_account import Credentials
+import streamlit as st
+from datetime import datetime
+from oauth2client.service_account import ServiceAccountCredentials
 
-def log_to_google_sheets(name, email, message, file_link, timestamp):
+def log_to_google_sheets(name, email, message, timestamp, filename="User Requests"):
     try:
-        scope = ["https://www.googleapis.com/auth/spreadsheets"]
-        credentials_dict = st.secrets["gspread"]
-        creds = Credentials.from_service_account_info(credentials_dict, scopes=scope)
+        # Use credentials from secrets.toml
+        scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+        credentials = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["gspread"], scope)
+        client = gspread.authorize(credentials)
 
-        client = gspread.authorize(creds)
-        sheet = client.open_by_key("1NByeYVPB0oX8i2ct9cEt3cQat7Dyp-uZxBbw17QiQeY")
+        # Open your sheet
+        sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1NByeYVPB0oX8i2ct9cEt3cQat7Dyp-uZxBbw17QiQeY/edit#gid=0")
         worksheet = sheet.sheet1
 
-        worksheet.append_row([name, email, message, file_link, timestamp])
-
+        # Add new row
+        worksheet.append_row([name, email, message, timestamp])
     except Exception as e:
         st.error(f"❌ Failed to log to Google Sheets: {e}")
