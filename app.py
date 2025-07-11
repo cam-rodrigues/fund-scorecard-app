b600e20b52776efc4137808aa8e7548c2297fad1
cam-rodrigues <crods611@gmail.com>
2025-07-11 19:43:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ st.markdown("""
         .beta-badge {
             position: absolute;
             top: 1.62rem;
-            left: 4.25rem;
+            left: 4.0rem;
             background-color: #2b6cb0;
             color: white;
             font-size: 0.48rem;
