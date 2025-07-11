910289a3d4c7c348c239e772f60d5b299783609a
cam-rodrigues <crods611@gmail.com>
2025-07-11 18:40:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ st.markdown("""
             align-items: center;
             margin-top: 1.0rem;
             margin-left: 0.3rem;
-            margin-right: 0rem;
+            margin-right: 0.0rem;
             width: 100%;
         }
 
