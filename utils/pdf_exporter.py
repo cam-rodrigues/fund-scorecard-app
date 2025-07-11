70d3d19a8dc61e583a5eea28607a5928e91eb32a
cam-rodrigues <crods611@gmail.com>
2025-07-10 21:38:47 -0400

    Update pdf_exporter.py

--- utils/pdf_exporter.py
+++ utils/pdf_exporter.py
@@ -1,32 +1,30 @@
-# utils/pdf_exporter.py
 from fpdf import FPDF
 from datetime import datetime
 
 class UniversalFidSyncPDF(FPDF):
     def header(self):
-        self.set_font("Helvetica", "B", 16)
+        self.set_font("Times", "B", 16)
         self.set_text_color(33, 37, 41)  # Dark gray
         self.set_fill_color(235, 241, 255)  # Light blue banner
         self.cell(0, 12, "FidSync Beta", ln=True, align="C", fill=True)
         self.set_text_color(0, 0, 0)
-        self.set_font("Helvetica", "", 9)
+        self.set_font("Times", "", 9)
         self.cell(0, 8, f"Generated {datetime.now().strftime('%B %d, %Y at %I:%M %p')}", ln=True, align="C")
         self.ln(4)
 
     def footer(self):
         self.set_y(-15)
-        self.set_font("Helvetica", "I", 8)
+        self.set_font("Times", "I", 8)
         self.set_text_color(128)
         self.cell(0, 10, f"Page {self.page_no()}/{{nb}}", align="C")
 
     def add_content(self, title, body_lines):
-        self.set_font("Helvetica", "B", 12)
+        self.set_font("Times", "B", 12)
         self.cell(0, 10, title, ln=True)
-        self.set_font("Helvetica", "", 11)
+        self.set_font("Times", "", 12)
         for line in body_lines:
-            self.multi_cell(0, 6, line)
-        self.ln(4)
-
+            self.multi_cell(0, 12, line)  # Double spacing
+        self.ln(8)
 
 def create_pdf_with_fidsync_banner(content_sections, output_path="fidsync_output.pdf"):
     """
@@ -45,6 +43,7 @@ def create_pdf_with_fidsync_banner(content_sections, output_path="fidsync_output
 
     pdf.output(output_path)
     return output_path
+
 
 #To Go Into Py's
 
