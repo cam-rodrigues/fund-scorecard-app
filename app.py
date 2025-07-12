543a00f846757859260ae8b20cbe73a73e75443c
cam-rodrigues <crods611@gmail.com>
2025-07-11 20:58:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,10 +19,10 @@ st.markdown("""
         [data-testid="stSidebar"]::after {
             content: "";
             position: absolute;
-            top: 8.0rem;
+            top: 8.5rem;
             right: 0;
             width: 2px;
-            height: calc(100% - 8.0rem);
+            height: calc(100% - 8.5rem);
             background-color: #b4c3d3;
             z-index: 1;
         }
