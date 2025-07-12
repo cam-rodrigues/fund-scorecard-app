0e1791c54d4234edf4ed4added139456dc0378c8
cam-rodrigues <crods611@gmail.com>
2025-07-12 14:21:10 -0400

    Update trusted_sources.py

--- app_pages/trusted_sources.py
+++ app_pages/trusted_sources.py
@@ -1,9 +1,4 @@
 import streamlit as st
-import base64
-from pathlib import Path
-
-def get_image_as_base64(path):
-    return base64.b64encode(Path(path).read_bytes()).decode()
 
 def run():
     st.set_page_config(page_title="Trusted Financial Sources", layout="wide")
@@ -61,52 +56,42 @@ def run():
     .source-box:hover .tooltip {
         display: block;
     }
-
-    hr {
-        border: none;
-        border-top: 1px solid #c3cfe0;
-        margin: 1.25rem auto 0.5rem auto;
-        width: 100%;
-    }
     </style>
     """, unsafe_allow_html=True)
 
     st.markdown("#### Financial News")
-
     render_sources([
-        ("https://www.bloomberg.com", "Bloomberg", "media/bloomberg.png"),
-        ("https://www.wsj.com", "Wall Street Journal", "media/wsj.png"),
-        ("https://www.cnbc.com", "CNBC", "media/cnbc.png"),
-        ("https://www.reuters.com", "Reuters", "media/reuters.png"),
+        ("https://www.bloomberg.com", "Bloomberg", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Bloomberg_logo.svg/2560px-Bloomberg_logo.svg.png"),
+        ("https://www.wsj.com", "Wall Street Journal", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/The_Wall_Street_Journal_Logo.svg/1280px-The_Wall_Street_Journal_Logo.svg.png"),
+        ("https://www.cnbc.com", "CNBC", "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/CNBC_logo.svg/2560px-CNBC_logo.svg.png"),
+        ("https://www.reuters.com", "Reuters", "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Reuters-Logo.svg/2560px-Reuters-Logo.svg.png"),
     ])
 
     st.markdown("#### Investment Research & Tools")
-
     render_sources([
-        ("https://www.morningstar.com", "Morningstar", "media/morningstar.png"),
-        ("https://www.ycharts.com", "YCharts", "media/ycharts.png"),
-        ("https://www.finviz.com", "Finviz", "media/finviz.png"),
-        ("https://www.fidelity.com", "Fidelity", "media/fidelity.png"),
-        ("https://www.schwab.com", "Charles Schwab", "media/schwab.png"),
-        ("https://www.emoneyadvisor.com", "eMoney Advisor", "media/emoney.png"),
+        ("https://www.morningstar.com", "Morningstar", "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Morningstar_Logo.svg/2560px-Morningstar_Logo.svg.png"),
+        ("https://www.ycharts.com", "YCharts", "https://assets-global.website-files.com/615c6125781dd69aafe19db1/618ac0d2f87c9637e3c3aa3e_ycharts-logo.png"),
+        ("https://www.finviz.com", "Finviz", "https://finviz.com/favicon.ico"),
+        ("https://www.fidelity.com", "Fidelity", "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Fidelity_Investments_logo.svg/2560px-Fidelity_Investments_logo.svg.png"),
+        ("https://www.schwab.com", "Charles Schwab", "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Charles_Schwab_Corporation_logo.svg/2560px-Charles_Schwab_Corporation_logo.svg.png"),
+        ("https://www.emoneyadvisor.com", "eMoney Advisor", "https://www.emoneyadvisor.com/wp-content/themes/emoney-2020/assets/images/emoney_logo.svg"),
     ])
 
     st.markdown("#### Government & Policy")
-
     render_sources([
-        ("https://www.sec.gov", "SEC", "media/sec.png"),
-        ("https://fred.stlouisfed.org", "FRED (St. Louis Fed)", "media/fred.png"),
-        ("https://www.finra.org", "FINRA", "media/finra.png"),
-        ("https://www.treasury.gov", "U.S. Treasury", "media/treasury.png"),
+        ("https://www.sec.gov", "SEC", "https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/Seal_of_the_United_States_Securities_and_Exchange_Commission.svg/2048px-Seal_of_the_United_States_Securities_and_Exchange_Commission.svg.png"),
+        ("https://fred.stlouisfed.org", "FRED (St. Louis Fed)", "https://fred.stlouisfed.org/~/media/Images/fred/fred-logo-blue.svg"),
+        ("https://www.finra.org", "FINRA", "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/FINRA_logo.svg/2560px-FINRA_logo.svg.png"),
+        ("https://www.treasury.gov", "U.S. Treasury", "https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Seal_of_the_United_States_Department_of_the_Treasury.svg/2048px-Seal_of_the_United_States_Department_of_the_Treasury.svg.png"),
     ])
+
 
 def render_sources(sources):
     st.markdown('<div class="source-grid">', unsafe_allow_html=True)
-    for url, name, logo_path in sources:
-        encoded = get_image_as_base64(logo_path)
+    for url, name, logo_url in sources:
         st.markdown(f"""
         <a href="{url}" target="_blank" class="source-box">
-            <img src="data:image/png;base64,{encoded}" alt="{name}" />
+            <img src="{logo_url}" alt="{name}" />
             <div class="tooltip">{name}</div>
         </a>
         """, unsafe_allow_html=True)
