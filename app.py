1c58eaea3572db6bfba1cf4e4e9760ef19a120a3
cam-rodrigues <crods611@gmail.com>
2025-07-12 09:09:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,8 @@ st.markdown("""
         .sidebar-logo-wrapper {
             margin-top: .15rem;
             margin-bottom: 1.5rem;
-            margin-left: 10 rem;
+            margin-left: 0.3rem;
+            padding-left: 0.4rem;
             display: flex;
             flex-direction: column;
             align-items: flex-start;
