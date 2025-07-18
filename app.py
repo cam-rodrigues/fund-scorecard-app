d1b6251f229ef1683366639e856de20b2982f6b0
cam-rodrigues <crods611@gmail.com>
2025-07-18 13:36:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -152,6 +152,7 @@ with st.sidebar.expander("MPI Tools", expanded=False):
 st.sidebar.markdown('<div class="sidebar-section">Under Construction</div>', unsafe_allow_html=True)
 nav_button("Proposal Generator", "proposal_generator.py")
 nav_button("Writeup Generator", "writeup_generator.py")
+nav_button("IPS Screening", "ips_investment_criteria.py")
 
 
 # === Page router ===
