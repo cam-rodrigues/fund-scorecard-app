bc44312c316fd80819c4e0fcd094a021cfd39d4b
cam-rodrigues <crods611@gmail.com>
2025-07-21 08:44:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -151,6 +151,7 @@ st.sidebar.markdown('<div class="sidebar-section">Under Construction</div>', uns
 nav_button("Proposal Generator", "proposal_generator.py")
 nav_button("Writeup Generator", "writeup_generator.py")
 nav_button("IPS Screening", "ips.py")
+nav_button("IPS Table", "ips_table.py")
 
 
 # === Page router ===
