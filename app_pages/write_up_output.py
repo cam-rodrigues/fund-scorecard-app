ab65d1d3ec77c52f83d4bb82672b367d726eca8e
cam-rodrigues <crods611@gmail.com>
2025-07-22 14:29:58 -0400

    Update write_up_output.py

--- app_pages/write_up_output.py
+++ app_pages/write_up_output.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from app_pages.write_up_processor import write_up_processor
+import write_up_processor
 
 def run():
     st.set_page_config(page_title="Fund IPS Summary", layout="wide")
