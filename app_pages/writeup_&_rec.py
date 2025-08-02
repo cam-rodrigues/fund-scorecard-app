d7364a5dac02f2bdaeac4e5631d869e3351561df
cam-rodrigues <crods611@gmail.com>
2025-08-02 19:04:02 -0400

    Update writeup_&_rec.py

--- app_pages/writeup_&_rec.py
+++ app_pages/writeup_&_rec.py
@@ -2098,7 +2098,7 @@ def run():
         # Step 14.5: IPS Fail Table
         step14_5_ips_fail_table()
 
-        step14_7_list_proposed_funds(pdf)
+        step14_7_list_proposed_funds_only(pdf)
         
         # Step 15: View Single Fund Details
         with st.expander("Single Fund Write Up", expanded=False):
